267773808b4bedc5f134cfbcfa473bec7d8e41c1
lasgranjas <78686390+lasgranjas@users.noreply.github.com>
2021-03-18 21:04:35 +0100

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,54 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 5 5 5 5 5 5 5 5 5 . . . . 
+. . . 5 1 1 5 5 5 1 1 5 . . . . 
+. . . 5 f 1 5 5 5 f 1 5 . . . . 
+. . . 5 5 5 5 5 5 5 5 5 . . . . 
+. . . 5 5 5 5 5 5 5 5 5 . . . . 
+. . . 5 5 5 2 2 2 5 5 5 . . . . 
+. . . 5 5 5 5 2 5 5 5 5 . . . . 
+. . . 5 5 5 5 5 5 5 5 5 . . . . 
+. . . e e e e e e e e e . . . . 
+. . . e e e . . . e e e . . . . 
+. . . e e e . . . e e e . . . . 
+. . . . 5 . . . . . 5 . . . . . 
+. . . e e . . . . . e e . . . . 
+`;
+            case "image2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 5 5 5 5 5 5 5 5 5 . . . . 
+. . . 5 1 1 5 5 5 1 1 5 . . . . 
+. . . 5 f 1 5 5 5 f 1 5 . . . . 
+. . . 5 5 5 5 5 5 5 5 5 . . . . 
+. . . 5 5 5 5 5 5 5 5 5 . . . . 
+. . . 5 5 5 2 2 2 5 5 5 . . . . 
+. . . 5 5 5 5 2 5 5 5 5 . . . . 
+. . . 5 5 5 5 5 5 5 5 5 . . . . 
+. . . e e e e e e e e e . . . . 
+. . . e e e . . . e e e . . . . 
+. . . e e e . . . e e e . . . . 
+. . . . 5 . . . . . 5 . . . . . 
+. . . e e . . . . . e e . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
